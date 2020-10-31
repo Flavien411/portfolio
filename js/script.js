59d6850f56a9c977023a5247cc508af3816657ff
Flav <flavien.coulmeau1@gmail.com>
2020-11-01 00:02:17 +0100

    Ajout animation text js + amélioration de la responsivité

--- js/script.js
+++ js/script.js
@@ -56,3 +56,35 @@ const showRequiredCategory = event => {
     getCategory.classList.add('showCategory')
 
 }
+
+function typeEffect(element, speed) {
+    var text = element.innerHTML;
+    element.innerHTML = "";
+
+    var i = 0;
+    var timer = setInterval(function() {
+        if (i < text.length) {
+            element.append(text.charAt(i));
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed);
+}
+
+
+// application
+var speed = 25;
+var h1 = document.querySelector('.pinfo');
+var p = document.querySelector('.pinfo');
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
+
+
+// type affect to body
+setTimeout(function() {
+    p.style.display = "inline-block";
+    typeEffect(p, speed);
+}, delay);
